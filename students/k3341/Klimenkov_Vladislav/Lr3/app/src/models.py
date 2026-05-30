3a56201b8aa5045ef5c2f01c7f5b5ecb882037d4
vlad <vkm.blog@yandex.ru>
2026-05-30 20:27:28 +0300

    Работа над ЛР3

--- students/k3341/Klimenkov_Vladislav/Lr3/app/src/models.py
+++ students/k3341/Klimenkov_Vladislav/Lr3/app/src/models.py
@@ -109,6 +109,7 @@ class User(SQLModel, table=True):
 class Skill(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(unique=True)
+    description: Optional[str] = Field(default=None)
     created_at: datetime = Field(default_factory=get_utc_now)
     updated_at: datetime = Field(default_factory=get_utc_now)
 
